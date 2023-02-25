b7c29046cb04db8c9f6f511f8954270b53a7bbcf
El-Behery <dev.elbehery@gmail.com>
2023-02-25 09:42:46 +0200

    done

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,8 +1,13 @@
 import "./header.scss";
 import logo from "../../assets/logo.png";
 import { Link, NavLink } from "react-router-dom";
+import MenuIcon from "@mui/icons-material/Menu";
+import CloseIcon from "@mui/icons-material/Close";
+import { useState } from "react";
 
 const Header = () => {
+  const [clicked, setClicked] = useState(false);
+
   return (
     <header>
       <div className="container">
@@ -11,25 +16,45 @@ const Header = () => {
             <img src={logo} alt="logo" />
           </Link>
         </div>
-        <nav>
+        <nav className={`${clicked ? "clicked" : ""}`}>
           <ul>
             <li>
-              <NavLink to={"/"}>Home</NavLink>
+              <NavLink onClick={() => setClicked(false)} to={"/"}>
+                Home
+              </NavLink>
             </li>
             <li>
-              <NavLink to={"about"}>About</NavLink>
+              <NavLink onClick={() => setClicked(false)} to={"about"}>
+                About
+              </NavLink>
             </li>
             <li>
-              <NavLink to={"courses"}>Courses</NavLink>
+              <NavLink onClick={() => setClicked(false)} to={"courses"}>
+                Courses
+              </NavLink>
             </li>
             <li>
-              <NavLink to={"blog"}>Blog</NavLink>
+              <NavLink onClick={() => setClicked(false)} to={"blog"}>
+                Blog
+              </NavLink>
             </li>
             <li>
-              <NavLink to={"contact"}>Contact</NavLink>
+              <NavLink onClick={() => setClicked(false)} to={"contact"}>
+                Contact
+              </NavLink>
             </li>
           </ul>
         </nav>
+        <MenuIcon
+          className={`menu menu-bar`}
+          onClick={() => setClicked(true)}
+          style={{ display: `${clicked ? "none" : "block"}` }}
+        />
+        <CloseIcon
+          className={`menu exit`}
+          onClick={() => setClicked(false)}
+          style={{ display: `${clicked ? "block" : "none"}` }}
+        />
       </div>
     </header>
   );
